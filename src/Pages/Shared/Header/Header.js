01e588a0325d821ae92ec03e56f23d3a9d491309
Thowhid31 <thowhidantor16@gmail.com>
2022-04-22 03:44:25 +0600

    added the final form

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -36,6 +36,7 @@ const Header = () => {
                         </Nav>
                         <Nav>
                             <Nav.Link as={Link} to="about">About</Nav.Link>
+                            
                             {
                                 user ?
                                     <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>sign out</button>
