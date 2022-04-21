44598b094a8db8081431f99fd72d987f60ee3165
Thowhid31 <thowhidantor16@gmail.com>
2022-04-22 02:13:11 +0600

    added form

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const AddService = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
     return (
-        <div>
+        <div className='w-50 mx-auto'>
             <h2>Please add a service.</h2>
+            <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+                <input className='mb-1' placeholder='name' {...register("name", { required: true, maxLength: 20 })} />
+                <textarea className='mb-1' placeholder='description' {...register("description")} />
+                <input className='mb-1' placeholder='price' type="number" {...register("price")} />
+                <input type="submit" />
+            </form>
         </div>
     );
 };
